f510da1b4a748b9694b495dee0fb5168920756d3
Padmesh Sharma <100578426+PadmeshSharma@users.noreply.github.com>
2023-05-09 12:26:24 +0530

    Update 0231-power-of-two.cpp

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -16,3 +16,23 @@ public:
         return false;
     }
 };
+
+
+
+/*
+class Solution {
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        for(int i=0;i<=30;i++)
+        {
+            int ans=pow(2,i);
+            if(ans==n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+*/
