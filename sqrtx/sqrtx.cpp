002f5f53fa3492f63721368b8d2451fc1c44e360
Padmesh Sharma <100578426+PadmeshSharma@users.noreply.github.com>
2023-10-17 01:48:57 +0530

    Time: 0 ms, Memory: 6.4 MB - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+    
+int mySqrt(int x) {
+    if (x <= 1) {
+        return x;
+    }
+
+    int left = 1, right = x / 2;
+    int result = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        long long square = static_cast<long long>(mid) * mid;
+
+        if (square == x) {
+            return mid;
+        } else if (square < x) {
+            left = mid + 1;
+            result = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
+    
+};
